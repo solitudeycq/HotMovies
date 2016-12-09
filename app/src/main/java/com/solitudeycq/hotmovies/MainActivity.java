1a0fa3b1d5a98e4a9253256108a4cdb1dcfbe84f
solitudeycq <solitudeycq@163.com>
2016-12-09 19:42:52 +0800

    完成首页fragment的相关配置

--- app/src/main/java/com/solitudeycq/hotmovies/MainActivity.java
+++ app/src/main/java/com/solitudeycq/hotmovies/MainActivity.java
@@ -1,13 +1,18 @@
 package com.solitudeycq.hotmovies;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new RecyclerViewFragment())
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/solitudeycq/hotmovies/RecyclerViewFragment.java
@@ -0,0 +1,10 @@
+package com.solitudeycq.hotmovies;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by solitudeycq on 2016/12/9.
+ */
+
+public class RecyclerViewFragment extends Fragment {
+}
