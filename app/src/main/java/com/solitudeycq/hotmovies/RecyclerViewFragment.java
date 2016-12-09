cf28880ac73f06f8388fc7f24b209997a093b25e
solitudeycq <solitudeycq@163.com>
2016-12-09 23:07:58 +0800

    加入默认图片

--- app/src/main/java/com/solitudeycq/hotmovies/RecyclerViewFragment.java
+++ app/src/main/java/com/solitudeycq/hotmovies/RecyclerViewFragment.java
@@ -3,6 +3,7 @@ package com.solitudeycq.hotmovies;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,6 +18,8 @@ import android.view.ViewGroup;
 
 public class RecyclerViewFragment extends Fragment {
     private static final String TAG = "RecyclerViewFragment";
+
+    private RecyclerView mRecyclerView;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +29,9 @@ public class RecyclerViewFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_main,container,false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.recyclerview_movies);
+        return v;
     }
 
     @Override
