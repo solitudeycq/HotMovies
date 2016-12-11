ec12230e9c08f18bb96ce073e8ca3df9992e29e7
solitudeycq <solitudeycq@163.com>
2016-12-11 11:05:47 +0800

    改个constant

--- app/src/main/java/com/solitudeycq/hotmovies/utils/Constants.java
+++ app/src/main/java/com/solitudeycq/hotmovies/utils/Constants.java
@@ -1,13 +1,10 @@
 package com.solitudeycq.hotmovies.utils;
 
-import com.solitudeycq.hotmovies.BuildConfig;
-
 /**
  * Created by solitudeycq on 2016/12/10.
  */
 
 public class Constants {
-    public static String API_KEY = BuildConfig.THEME_MOVIE_DB_API_KEY;
     public static String GET_PICTURE_BASIC_URL = "http://image.tmdb.org/t/p/w185";
     public static String GET_MOVIES_BY_POPULAR = "http://api.themoviedb.org/3/movie/popular?language=zh&api_key=";
     public static String GET_MOVIES_BY_TOPRATED = "http://api.themoviedb.org/3/movie/top_rated?language=zh&api_key=";
