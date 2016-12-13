63ea2cdeed46ab4f1b3b6d20cbd65998ba6b4568
solitudeycq <solitudeycq@163.com>
2016-12-13 15:43:52 +0800

    设置菜单

--- app/src/main/java/com/solitudeycq/hotmovies/activity/SettingsActivity.java
+++ app/src/main/java/com/solitudeycq/hotmovies/activity/SettingsActivity.java
@@ -1,17 +1,49 @@
 package com.solitudeycq.hotmovies.activity;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 import com.solitudeycq.hotmovies.R;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        addPreferencesFromResource(R.xml.pref_general);
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_searchBy_key)));
     }
 
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object value) {
+        String stringValue = value.toString();
+        if (preference instanceof ListPreference) {
+            // For list preferences, look up the correct display value in
+            // the preference's 'entries' list (since they have separate labels/values).
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        } else {
+            // For other preferences, set the summary to the value's simple string representation.
+            preference.setSummary(stringValue);
+        }
+        return true;
+    }
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        // Set the listener to watch for value changes.
+        preference.setOnPreferenceChangeListener(this);
+
+        // Trigger the listener immediately with the preference's
+        // current value.
+        onPreferenceChange(preference,
+                PreferenceManager
+                        .getDefaultSharedPreferences(preference.getContext())
+                        .getString(preference.getKey(), ""));
+    }
 }
